b9e8c0debe23615a912f7fa52c8600ae2c3246c4
MariiaKytsmurDQE <Mariia_Kytsmur@epam.com>
2023-02-13 21:05:44 +0200

    Hometask_1/Hometask_1.py

--- Hometask_1/Hometask_1.py
+++ Hometask_1/Hometask_1.py
@@ -7,7 +7,7 @@ for _ in range(100):  # Define count of numbers in the list
 # Sort list from min to max
 sorted_numbers = []  # Create a new list for further sorted numbers from min to max
 while hundred_random_numbers:
-    minimum = hundred_random_numbers[0]  # Put min value on the beginning of the list
+    minimum = hundred_random_numbers[0]
     for number in hundred_random_numbers:
         if number < minimum:
             minimum = number
@@ -16,13 +16,13 @@ while hundred_random_numbers:
 # Calculate AVG for evens and odds number from the list
 even_numbers = []  # Create an empty list for further count of even numbers
 odd_numbers = []  # Create an empty list for further count of odd numbers
-for i in sorted_numbers:
-    if i % 2 == 0:  # Check whether the element of list is even number
-        even_numbers.append(i)  # Append every even number to list 'even'
-        even_average = round(sum(even_numbers) / len(even_numbers))
-    elif i % 2 == 1:  # Check whether the element of list is odd number
-        odd_numbers.append(i)  # Append every even number to list 'odd'
-        odd_average = round(sum(odd_numbers) / len(odd_numbers))
+for number in sorted_numbers:
+    if number % 2 == 0:  # Check whether the element of list is even number
+        even_numbers.append(number)  # Append every even number to list 'even'
+    elif number % 2 == 1:  # Check whether the element of list is odd number
+        odd_numbers.append(number)  # Append every even number to list 'odd'
+even_average = round(sum(even_numbers) / len(even_numbers))
+odd_average = round(sum(odd_numbers) / len(odd_numbers))
 
 print('Sorted numbers from min to max: ', sorted_numbers)
 print('Average of even numbers: ', even_average)
