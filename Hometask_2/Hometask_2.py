7b11c3056b47e9897ca9b21aeedf7db422c6d739
MariiaKytsmurDQE <Mariia_Kytsmur@epam.com>
2023-02-22 21:12:00 +0200

    Hometask_2/Hometask_2.py

--- Hometask_2/Hometask_2.py
+++ Hometask_2/Hometask_2.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 import random
 import string
 
@@ -6,35 +5,38 @@ import string
 # Create the list dictionaries from 2 to 10.
 def create_dicts():
     output = []  # Dicts list
-    for dict_number in range(random.randint(2, 10)):  # Dictionary counter
+    for _ in range(random.randint(2, 10)):  # Dictionary counter
         dictionary = {}
-        for elem_number in range(random.randint(1, 26)):  # Elements counter
+        for _ in range(random.randint(1, 26)):  # Elements counter
             # Key(random letter), value(random number from 0 to 100)
             dictionary[random.choice(string.ascii_lowercase)] = random.randint(0, 100)
         output.append(dictionary)  # Append every dictionary to the list
     return output
 
 
-output = defaultdict(lambda: float('-inf'))
+output = {}
 
 list_of_dicts = create_dicts()
 
+c = 1
+for cntr in list_of_dicts:
+    for key, value in cntr.items():
+        if key not in output:  # Check whether value exists previously
+            output[key] = {str(c): value}
+        else:
+            output[key][str(c)] = value
+    c += 1
 
-for cntr in range(0, len(list_of_dicts)):  # Create a counter for later adding of '_number_of_dictionary' for the same keys
-    for key, value in list_of_dicts[cntr].items():
-        if output[key] != float('-inf'):  # Check whether value exists previously
-            if output[key] > value:  # If stored value > actual than we do nothing
-                continue
-            del output[key]  # Delete key with max value
-            key = f'{key}_{cntr + 1}'  # Create new key with adding '_number_of_dictionary'
-
-        output[key] = max(output[key], value)  # Add key: value
+my_dict = {}
+for key, value in output.items():
+    if len(output[key]) == 1:
+        my_dict[key] = max(output[key].values())
+    else:
+        my_dict[key + "_" + str(max(output[key], key=output[key].get))] = max(output[key].values())
 
 
-for i in range(0, len(list_of_dicts)):
+for i in range(len(list_of_dicts)):
     print(f'Dict {i+1}:  {list_of_dicts[i]}')  # Just for better displaying
 print('')
-print('dictionary with max value:', dict(output))
-
-
-
+print('dictionary with all value for key:', dict(output))
+print('Final dictionary:', my_dict)
